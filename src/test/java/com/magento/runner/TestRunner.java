c2cbfd5b6c2a61e960d20758ce3ab9ac06d48ef4
Luis Ruiz <lruizajax@gmail.com>
2023-08-19 18:02:00 -0500

    change to junit

--- src/test/java/com/magento/runner/TestRunner.java
+++ src/test/java/com/magento/runner/TestRunner.java
@@ -1,10 +1,11 @@
 package com.magento.runner;
 
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.DataProvider;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
+@RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features/",
         glue = "com.magento", // recordar el path de hooks
@@ -20,11 +21,5 @@ import org.testng.annotations.DataProvider;
         //tags = "@Login",
         dryRun = false
 )
-public class TestRunner extends AbstractTestNGCucumberTests {
-
-        /*@Override
-        @DataProvider(parallel = true)
-        public Object[][] scenarios() {
-                return super.scenarios();
-        }*/
+public class TestRunner  {
 }
